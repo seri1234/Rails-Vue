af5d87206c7883d84adf86c7b19769dfb6605a56
Your Name <your.email@example.com>
2019-05-23 13:55:30 +0000

    Update Gemfile for Heroku

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,32 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div>
+    <input v-model="msgBox" placeholder="message here"></input>
+    <button v-if="messageChannel" @click="speak">送信</button>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
+data() {
+  return {
+    msgBox: "",
+    messages: [],
+    messageChannel: null,
+  };
+},
+created() {
+  this.messageChannel = this.$cable.subscriptions.create( "messageChannel",{
+    received: (data) => {
+      this.messages.push(data)
+    },
+  })
+},
+methods: {
+  speak() {
+    this.messageChannel.perform('speak', { 
+    message: msgBox,
+    });
+  },
+},
 }
 </script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  mount ActionCable.server => '/cable'
 end
